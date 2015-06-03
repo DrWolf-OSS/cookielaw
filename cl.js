43c827dd6e77369044839727a48713664dd9b7e5
Andrea Agili <agea@drwolf.it>
2015-06-03 12:18:25 +0200

    style

--- cl.js
+++ cl.js
@@ -15,6 +15,7 @@ cl.getCookie = function (c_name) {
 cl.acceptCookies = function () {
     cl.setCookie("jsCookieCheck", "OK", 365);
     cl.showCookieNotification = false;
+    document.getElementsByTagName('body')[0].removeChild(cl.div);
 };
 
 cl.setCookie = function (c_name, value, exdays) {
@@ -28,16 +29,18 @@ cl.checkCookie = function () {
     var cookieName = "jsCookieCheck";
     var cookieChk = cl.getCookie(cookieName);
     if (cookieChk == "OK") {
-        setCookie(cookieName, cookieChk, 365);
+        cl.setCookie(cookieName, cookieChk, 365);
     } else {
         cl.div = document.createElement("div");
-        cl.div.innerHTML = '<p style="color:white">Su questo sito utilizziamo cookie tecnici e per raccogliere '+
-            'in modo anonimo statistiche sugli accessi<p>'+
-            '<a href="javascript:void(0)" onclick="cl.acceptCookies()">OK</a>';
+        cl.div.innerHTML = '<p style="text-align: center;color: white;padding: 1em;">'+
+            'Su questo sito utilizziamo cookie tecnici e per raccogliere '+
+            'in modo anonimo statistiche sugli accessi</p>'+
+            '<p style="text-align:center">'+
+            '<a style="color: black;background: white;padding: .5em 2em;border: 1px solid black;" href="javascript:void(0)" onclick="cl.acceptCookies()">OK</a></p>';
         document.getElementsByTagName('body')[0].appendChild(cl.div);
         cl.div.style.position='fixed';
         cl.div.style.width="100%";
-        cl.div.style.background="rgba(0,0,0,0.7)";
+        cl.div.style.background="rgba(0,0,0,0.75)";
         
     }
 };
