c55df8afea339463abfb5d925ee86fd904e9a2d0
Andrea Agili <agea@drwolf.it>
2015-10-15 08:27:43 +0200

    ui update, a#cookie-policy check

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # cookielaw
 small javascript to alert users about cookies
 
-insert this code **inside** the `body` tag
+insert this code **at the end** of the `body` tag
 
 ```html
-<script type="text/javascript" src="https://cdn.rawgit.com/DrWolf-OSS/cookielaw/2.0/cl.js"></script>
+<script type="text/javascript" src="https://cdn.rawgit.com/DrWolf-OSS/cookielaw/2.1/cl.js"></script>
 ```
+
+if you have an `a` tag with id `cookie-policy` the script will point to its `href`
--- cl.js
+++ cl.js
@@ -1,53 +1,65 @@
 var cl = cl || {};
 
 cl.getCookie = function (c_name) {
-    var i, x, y, ARRcookies = document.cookie.split(";");
-    for (i = 0; i < ARRcookies.length; i++) {
-        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-        x = x.replace(/^\s+|\s+$/g, "");
-        if (x == c_name) {
-            return unescape(y);
-        }
+  var i, x, y, ARRcookies = document.cookie.split(";");
+  for (i = 0; i < ARRcookies.length; i++) {
+    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+    y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+    x = x.replace(/^\s+|\s+$/g, "");
+    if (x == c_name) {
+      return unescape(y);
     }
+  }
 };
 
 cl.acceptCookies = function () {
-    cl.setCookie("jsCookieCheck", "OK", 365);
-    cl.showCookieNotification = false;
-    document.getElementsByTagName('body')[0].removeChild(cl.div);
+  cl.setCookie("jsCookieCheck", "OK", 365);
+  cl.showCookieNotification = false;
+  document.getElementsByTagName('body')[0].removeChild(cl.div);
 };
 
 cl.setCookie = function (c_name, value, exdays) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) + ((exdays === null) ? "" : "; expires=" + exdate.toUTCString() + "; path=/");
-    document.cookie = c_name + "=" + c_value;
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value = escape(value) + ((exdays === null) ? "" : "; expires=" + exdate.toUTCString() + "; path=/");
+  document.cookie = c_name + "=" + c_value;
 };
 
 cl.checkCookie = function () {
-    var cookieName = "jsCookieCheck";
-    var cookieChk = cl.getCookie(cookieName);
-    if (cookieChk == "OK") {
-        cl.setCookie(cookieName, cookieChk, 365);
-    } else {
-        cl.div = document.createElement("div");
-        cl.div.innerHTML = 
-            '<p style="text-align: center;color: white;padding: 1em;">'+
-            'Su questo sito utilizziamo cookie per motivi tecnici e per raccogliere '+
-            'in modo anonimo statistiche sugli accessi</p>'+
-            '<p style="text-align: center;color: white;padding: 1em; border-top:1px dashed white;">'+
-            'On this site we use cookies for technical purposes and to collect '+
-            'anonymous data about site visits</p>'+
-            '<p style="text-align:center">'+
-            '<a style="color: black;background: white;padding: .5em 2em;border: 1px solid black;" href="javascript:void(0)" onclick="cl.acceptCookies()">OK</a></p>';
-        document.getElementsByTagName('body')[0].appendChild(cl.div);
-        cl.div.style.position='fixed';
-        cl.div.style.bottom='0';
-        cl.div.style.width="100%";
-        cl.div.style.background="rgba(0,0,0,0.75)";
-        
+  var cookieName = "jsCookieCheck";
+  var cookieChk = cl.getCookie(cookieName);
+  if (cookieChk == "OK") {
+    cl.setCookie(cookieName, cookieChk, 365);
+  } else {
+    cl.div = document.createElement("div");
+    var it = "Utilizzando questo sito, accetti la nostra ";
+    var en = "By using this site, you accept our ";
+    var cpl = "https://support.google.com/analytics/answer/6004245";
+    var el = document.getElementById('cookie-policy');
+    if (el) {
+      cpl = el.href;
     }
+
+    cl.div.innerHTML =
+      '<p style="text-align: left;color: white;padding: .75em .75em 0em .75em;">' +
+      it +
+      '<a href="' + cpl + '" >Cookie Policy</a></p>' +
+      '<p style="text-align: left;color: white;padding: .5em .75em 0em .75em; border-top:1px dashed #444;margin-right:42px">' +
+      en +
+      '<a target="_blank" href="' + cpl + '" >Cookie Policy</a></p>' +
+      '<p style="text-align:center">' +
+      '<a style="text-decoration:none;color: black;background: white;padding: 5px;border: 0;position: absolute;right: 5px;bottom: 5px;width: 32px;" href="javascript:void(0)" onclick="cl.acceptCookies()">OK</a></p>';
+    cl.div.style.position = 'fixed';
+    cl.div.style.right = '15px';
+    cl.div.style.bottom = '15px';
+    cl.div.style.background = "rgba(0,0,0,0.85)";
+    cl.div.style.fontSize = '75%';
+    cl.div.style.borderRadius = '2px';
+    cl.div.style.boxShadow = '1px 1px 2px rgba(0,0,0,0.3)';
+
+    document.getElementsByTagName('body')[0].appendChild(cl.div);
+
+  }
 };
 
 cl.checkCookie();
